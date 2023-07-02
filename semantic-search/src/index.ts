802f256d311b7ec73e98590d3dbf018e389d55cc
dtellz <dtellez95@gmail.com>
2023-07-02 15:38:10 +0100

    add indexer and sample file

---
+++ semantic-search/src/index.ts
@@ -0,0 +1,61 @@
+import { utils } from '@pinecone-database/pinecone';
+import cliProgress from 'cli-progress';
+import { config } from 'dotenv';
+import { loadCsvFile } from './csvLoader.js';
+import { embedder } from './embeddings.js';
+import { getPineconeClient } from './pinecode.js';
+import { getEnv, getIndexingCliArgs } from './utils/utils.js';
+const { createIndexIfNotExists, chunkedUpsert } = utils;
+config();
+
+
+const progressBar = new cliProgress.SingleBar(
+    {},
+    cliProgress.Presets.shades_classic,
+);
+
+const indexName = getEnv('PINECONE_INDEX');
+let counter = 0;
+
+const run = async () => {
+    // Get the CSV path and column name from the CLI args
+    const { csvPath, column } = getIndexingCliArgs();
+
+    // Get the Pinecone client instance
+    const pineconeClient = await getPineconeClient();
+
+    // Create a readable stream from the CSV file
+    const { data, meta } = await loadCsvFile(csvPath);
+
+    // Ensure the selected colum exists in the CSV
+    if (!meta.fields?.includes(column)) {
+        console.error(`Column ${column} not found in CSV`);
+        process.exit(1);
+    }
+
+    // Extract the selected column from the CSV
+    const documents = data.map((row) => row[column] as string);
+
+    // Create the pinecone index if it doesn't exist with the expected dimension of the 'Xenova/all-MiniLM-L6-v2' model embeddings
+    await createIndexIfNotExists(pineconeClient, indexName, 384);
+
+    // select the target index
+    const index = pineconeClient.Index(indexName);
+
+    // Start the progress bar
+    progressBar.start(documents.length, 0);
+
+    // Start the batch embedding process
+    await embedder.init();
+    await embedder.embedBatch(documents, 1000, async (embeddings) => {
+        // Upsert the embeddings into the index
+        await chunkedUpsert(index, embeddings, 'default');
+        counter += embeddings.length;
+        progressBar.update(counter);
+    });
+
+    progressBar.stop();
+    console.log(`Indexed ${counter} documents into ${indexName}`);
+}
+
+run();
--- semantic-search/src/utils/utils.ts
+++ semantic-search/src/utils/utils.ts
@@ -1,4 +1,5 @@
-
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
 
 const sliceIntoChunks = <T>(arr: T[], chunkSize: number) => {
     return Array.from({ length: Math.ceil(arr.length / chunkSize) }, (_, i) =>
@@ -20,5 +21,31 @@ const validateEnvVars = () => {
     getEnv("PINECONE_INDEX");
 };
 
+const getIndexingCliArgs = () => {
+    const argv = yargs(hideBin(process.argv))
+        .option("csvPath", {
+            alias: "p",
+            type: "string",
+            description: "Path to your CSV path",
+            demandOption: true,
+        })
+        .option("column", {
+            alias: "c",
+            type: "string",
+            description: "The name for the CSV column",
+            demandOption: true,
+        })
+        .parseSync();
 
-export { sliceIntoChunks, getEnv, validateEnvVars };
+    const { csvPath, column } = argv;
+
+    if (!csvPath) {
+        console.error("Please provide a CSV path");
+        process.exit(1);
+    }
+
+    return { csvPath, column };
+};
+
+
+export { sliceIntoChunks, getEnv, validateEnvVars, getIndexingCliArgs };
