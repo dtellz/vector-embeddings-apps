b3b00d322b8b19da6e5a7dd021340034b9278494
dtellz <dtellez95@gmail.com>
2023-07-02 11:30:15 +0100

    add csvLoader and create util chunk slicer

---
+++ semantic-search/src/csvLoader.ts
@@ -0,0 +1,18 @@
+import fs from 'fs/promises';
+import Papa from 'papaparse';
+
+export const loadCsvFile = async (filePath: string): Promise<Papa.ParseResult<Record<string, unknown>>> => {
+    try {
+        const csvAbsPath = await fs.realpath(filePath);
+        const data = await fs.readFile(csvAbsPath, 'utf8');
+        return Papa.parse(data, {
+            dynamicTyping: true,
+            header: true,
+            skipEmptyLines: true,
+        });
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
---
+++ semantic-search/src/embeddings.ts
@@ -0,0 +1,8 @@
+import { Vector } from '@pinecone-database/pinecone';
+import { Pipeline } from '@xenova/transformers';
+import { v4 as uuidv4 } from 'uuid';
+import { sliceIntoChunks } from './utils/utils.js';
+
+class Embedder {
+
+}
