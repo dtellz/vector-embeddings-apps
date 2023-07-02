fb509e6bac26f239bd18b630948e2d71cb4353c8
dtellz <dtellez95@gmail.com>
2023-07-02 12:52:07 +0100

    add batch embedder and fix sliceIntoChunks

--- semantic-search/src/utils/utils.ts
+++ semantic-search/src/utils/utils.ts
@@ -1,9 +1,9 @@
 
 
 const sliceIntoChunks = <T>(arr: T[], chunkSize: number) => {
-    return Array.from({ length: Math.ceil(arr.length / chunkSize) }, (_, i) => {
-        arr.slice(i * chunkSize, (i + 1) * chunkSize);
-    })
-}
+    return Array.from({ length: Math.ceil(arr.length / chunkSize) }, (_, i) =>
+        arr.slice(i * chunkSize, (i + 1) * chunkSize)
+    );
+};
 
 export { sliceIntoChunks };
